624bf70de583c4276e3b97ce693503687e6dd404
ChaoticWeg <shawn.lutch94@gmail.com>
2016-06-07 02:22:54 -0500

    convert all tabs to spaces
    it will always be spaces

--- index.js
+++ index.js
@@ -7,34 +7,34 @@ var audioDir = path.join(__dirname, 'sounds');
 var bot      = Discord.getInstance();
 
 process.on('SIGINT', () => {
-	console.log('\nStopping gracefully.');
+  console.log('\nStopping gracefully.');
 
-	console.log('Logging out...')
-	bot.logout(() => {
-		console.log('Logged out. Exiting...');
-		process.exit(0);
-	});
+  console.log('Logging out...')
+  bot.logout(() => {
+    console.log('Logged out. Exiting...');
+    process.exit(0);
+  });
 });
 
 Audio.loadAllFiles(audioDir, (err) => {
 
-	if (err) {
-		// if error encountered while loading, print error and bail out entirely
-		console.error('[ERROR] unable to load audio files!');
-		console.error('[ERROR]', err);
+  if (err) {
+    // if error encountered while loading, print error and bail out entirely
+    console.error('[ERROR] unable to load audio files!');
+    console.error('[ERROR]', err);
 
-		process.exit(1);
-	}
+    process.exit(1);
+  }
 
-	console.log('[INDEX] done loading %d audio files into registry', Audio.Registry.count());
+  console.log('[INDEX] done loading %d audio files into registry', Audio.Registry.count());
 
-	/* we have successfully loaded the audio files. log in. */
+  /* we have successfully loaded the audio files. log in. */
 
-	bot.login(bot.__creds.email, bot.__creds.pass, (err) => {
-		if (err) bot.__events.onLoginError(bot, err);
+  bot.login(bot.__creds.email, bot.__creds.pass, (err) => {
+    if (err) bot.__events.onLoginError(bot, err);
 
-		/* we have successfully logged in. TODO other stuff */
-		console.log('[DSCRD] logged in successfully.');
-	});
+    /* we have successfully logged in. TODO other stuff */
+    console.log('[DSCRD] logged in successfully.');
+  });
 
 });
